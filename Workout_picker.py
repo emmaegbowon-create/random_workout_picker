43ca37d28b509b546ef3f39a28b902e6302cf435
Oluwatobi Emmanuel Egbowon <emmaegbowon@gmail.com>
2026-08-27 02:51:34 +0100

    Add files via upload

---
+++ Workout_picker.py
@@ -0,0 +1,6 @@
+import random
+
+activities = ["Push-ups", "Sit-ups", "Squats", "Lunges",
+              "Plank", "Burpees", "Jumping Jacks", "Mountain Climbers"]
+todo = random.choice(activities)
+print("todays workout is: " + todo)
